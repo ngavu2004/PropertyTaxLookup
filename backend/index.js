77e04f53b768e83acbb1ae388732a5d89a74f284
ngavu2004 <vuthiquynhngahvn@gmail.com>
2025-10-25 00:39:40 -0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: process.env.TRUSTED_ORIGINS.split(','),
+  origin: process.env.CORS_TRUSTED_ORIGIN.split(','),
 };
 app.use(cors(corsOptions));
 app.use(express.json());
