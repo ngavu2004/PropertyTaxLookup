249ca56d198ab00ab3ae81bfbdec5023d8a490c2
ngavu2004 <vuthiquynhngahvn@gmail.com>
2025-10-25 00:37:29 -0700

    UPDATE cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.TRUSTED_ORIGINS.split(','),
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
